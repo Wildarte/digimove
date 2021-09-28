08419bce5510d7864bc2fb463058327546888219
wildarte <wildarte10@gmail.com>
2021-09-28 20:46:39 -0300

    commit da importacao da lib de icons do bootstrap

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/font/bootstrap-icons.css">
+    <style>
+        i{
+            font-size: 1.5em;
+        }
+    </style>
+</head>
+<body>
+    
+    <header>
+        <div>
+            <div>
+                <a href="#">
+                    <img src="" alt="logo do site">
+                </a>
+            </div>
+        </div>
+        <div>
+            <div>
+                <a href="#"><i class="bi bi-facebook"></i>facebook</a>
+                <a href="#"><i class="bi bi-instagram"></i>instagram</a>
+                <a href="#"><i class="bi bi-twitter"></i>twitter</a>
+                <a href="#"><i class="bi bi-linkedin"></i>linkedin</a>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Imóveis</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+</html>
